ff24663e704dcac6811f3d81be3a08cb07f72a4a
jeremyfouquet <91273814+jeremyfouquet@users.noreply.github.com>
2023-02-23 16:14:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 # Version ..... : V1 du 12/05/2022
 # Licence ..... : réalisé dans le cadre du cours de Gilles Bernard pour la licence 2 Informatique de L'IED Paris 8
 # Compilation : g++ *.cpp -o dictionnaire -Wall -lX11 -lXm -lXt
-# Usage : Pour exécuter : ./dictionnaire
+# Usage : ./dictionnaire
 ********************************************************/
 
 #include <stdio.h>
